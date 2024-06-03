a2c55bad37acdd082a784d5ab13c873e88bfaf20
sarthak singh <buddysuccess7@gmail.com>
2024-06-03 10:34:36 +0530

    done lecture 3

--- App.js
+++ App.js
@@ -1,32 +1,45 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
 //this is only one element
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world using ReactJs!!"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world using ReactJs!!"
+// );
 
-//how we show nested element and how it is works
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "innerdiv" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("p", {}, "I am p tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "innerdiv2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("p", {}, "I am p tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "innerdiv3" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("p", {}, "I am p tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-console.log(heading); //its basically returns the object.
+// //how we show nested element and how it is works
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "innerdiv" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("p", {}, "I am p tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "innerdiv2" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("p", {}, "I am p tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "innerdiv3" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("p", {}, "I am p tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+// ]);
+
+//component composition (in a one component adding more than 2 component)
+const Title = () => {
+  return <h1 id="heading">Hello world using ReactJs!!</h1>;
+};
+
+const MainHeading = () => {
+  return (
+    <>
+      <Title />
+      <h1 id="heading">Hello world using ReactJs!! again</h1>;
+    </>
+  );
+};
+console.log(Title); //its basically returns the object.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-root.render(parent);
+root.render(MainHeading);
